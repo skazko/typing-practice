163fb3bf29e285c8eb77441eee37ab8c505b08b5
Kazachenko Viacheslav <kreeon@ya.ru>
2021-03-07 01:17:18 +0300

    fix: email pattern

--- src/entities/credentials.ts
+++ src/entities/credentials.ts
@@ -3,7 +3,7 @@ export type MaybeCredentials = {
   password: string;
 };
 
-const emailPattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+const emailPattern = /^[\w\.]+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
 
 export class Credentials {
   static hasEmail(credentials: MaybeCredentials): boolean {
